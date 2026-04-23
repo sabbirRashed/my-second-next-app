d9609c4170df996b4609199539c78a79e816d3f1
sabbirRashed <srsabbirrahman12@gmail.com>
2026-04-23 22:48:04 +0600

    nextjs environment setup

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    console.log("I am from server")
+    return (
+        <div>
+            <h2>This is the header</h2>
+        </div>
+    );
+};
+
+export default Header;
